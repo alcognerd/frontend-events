f71a5d7d8db3d45a80062c4d8bb2735c76f07fd9
suryacp23 <suryacp.com@gmail.com>
2025-02-15 10:32:02 +0530

    added another app

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,157 +6,165 @@ import { useMutation, useQuery } from "@tanstack/react-query";
 import { fetchRoles, signout } from "../services/api";
 
 const Header = () => {
-	const location = useLocation();
-	const [hovered, setHovered] = useState(null);
-	const [menuOpen, setMenuOpen] = useState(false);
-	const { user } = useAuth();
-	const [role, setRole] = useState("member");
+  const location = useLocation();
+  const [hovered, setHovered] = useState(null);
+  const [menuOpen, setMenuOpen] = useState(false);
+  const { user } = useAuth();
+  const [role, setRole] = useState("member");
 
-	const { data } = useQuery({
-		queryKey: ["role", user?.id],
-		queryFn: fetchRoles,
-		enabled: !!user,
-	});
+  const { data } = useQuery({
+    queryKey: ["role", user?.id],
+    queryFn: fetchRoles,
+    enabled: !!user,
+  });
 
-	useEffect(() => {
-		if (data) {
-			setRole(data);
-		}
-	}, [data]);
+  useEffect(() => {
+    if (data) {
+      setRole(data);
+    }
+  }, [data]);
 
-	const navLinks = [
-		{ label: "Events", href: "/events" },
-		{ label: "Chats", href: "/chats" },
-		{ label: "Create Events", href: "/create-events" },
-		{ label: "About", href: "/about" },
-	];
+  const navLinks = [
+    { label: "Events", href: "/events" },
+    { label: "Chats", href: "/chats" },
+    { label: "Create Events", href: "/create-events" },
+    { label: "About", href: "/about" },
+  ];
 
-	if (role?.host) {
-		navLinks.push({ label: "Dashboard", href: "/dashboard" });
-	} else if (role?.moderator) {
-		navLinks.push({ label: "Request", href: "/request" });
-	}
-	const navigate = useNavigate();
+  if (role?.host) {
+    navLinks.push({ label: "Dashboard", href: "/dashboard" });
+  } else if (role?.moderator) {
+    navLinks.push({ label: "Request", href: "/request" });
+  }
+  const navigate = useNavigate();
 
-	const { mutate, isPending } = useMutation({
-		mutationFn: signout,
-		onSuccess: () => {
-			localStorage.removeItem("user");
-			navigate("/login");
-		},
-	});
-	const handleLogout = () => {
-		mutate(null, {
-			onSuccess: () => {
-				localStorage.removeItem("token");
-				navigate("/login");
-			},
-		});
-	};
+  const { mutate, isPending } = useMutation({
+    mutationFn: signout,
+    onSuccess: () => {
+      localStorage.removeItem("user");
+      navigate("/login");
+    },
+  });
+  const handleLogout = () => {
+    mutate(null, {
+      onSuccess: () => {
+        localStorage.removeItem("token");
+        navigate("/login");
+      },
+    });
+  };
 
-	return (
-		<header className="p-5 z-10 max-w-[1440px] mx-auto text-lavender_bush">
-			<nav className="flex justify-between items-center">
-				<Link to={"/events"} className=" scale-125">
-					SUE
-				</Link>
-				{/* Desktop Navigation */}
-				<ul className="flex justify-center items-center gap-16 max-lg:hidden relative">
-					{navLinks.map((item) => (
-						<li
-							key={item?.label}
-							onMouseEnter={() => setHovered(item.href)}
-							onMouseLeave={() => setHovered(null)}
-							className="relative"
-						>
-							<Link to={item?.href} className={` text-lg text-white capitalize`}>
-								{item?.label}
-							</Link>
-							<span
-								className={`absolute left-0 bottom-[-8px] h-[5px] rounded-full bg-purple-500 transition-all duration-300 ${
-									location?.pathname === item?.href || hovered === item?.href
-										? "w-full"
-										: "w-0"
-								} ${location?.pathname === item?.href ? "bg-white" : ""}`}
-							></span>
-						</li>
-					))}
-					{user ? (
-						<UserMenu />
-					) : (
-						<li
-							onMouseEnter={() => setHovered("/login")}
-							onMouseLeave={() => setHovered(null)}
-							className="relative"
-						>
-							<Link to={"/login"} className="text-lg text-white capitalize">
-								Login
-							</Link>
-							<span
-								className={`absolute left-0 bottom-[-8px] h-[5px] rounded-full bg-purple-500 transition-all duration-300 ${
-									hovered === "/login" ? "w-full" : "w-0"
-								}`}
-							></span>
-						</li>
-					)}
-				</ul>
-				{/* Hamburger Menu Button */}
-				<div className="hidden max-lg:block">
-					<button className="text-white text-lg" onClick={() => setMenuOpen(!menuOpen)}>
-						☰
-					</button>
-				</div>
-			</nav>
-			{/* Sliding Full-Screen Menu */}
-			<div
-				className={`fixed top-0 left-0 z-50 h-full w-full bg-[#1f1f1f] text-white p-5 transform transition-transform duration-300 ${
-					menuOpen ? "translate-x-0" : "-translate-x-full"
-				}`}
-			>
-				<button
-					className="text-lg font-bold absolute top-4 right-4"
-					onClick={() => setMenuOpen(false)}
-				>
-					✕
-				</button>
-				<ul className="flex flex-col items-center justify-evenly h-full gap-6">
-					{navLinks.map((item) => (
-						<li key={item?.label} className="text-lg w-full">
-							<Link
-								to={item?.href}
-								className={`capitalize hover:text-purple-500 ${
-									location?.pathname === item?.href ? "text-purple-500 font-bold" : ""
-								}`}
-								onClick={() => setMenuOpen(false)} // Close menu on link click
-							>
-								{item?.label}
-							</Link>
-						</li>
-					))}
-					{user ? (
-						<li className="w-full">
-							<button
-								className="text-red-50 bg-red-950 p-2 w-full text-lg"
-								onClick={handleLogout}
-							>
-								sign out
-							</button>
-						</li>
-					) : (
-						<li
-							onMouseEnter={() => setHovered("/login")}
-							onMouseLeave={() => setHovered(null)}
-							className="relative"
-						>
-							<Link to={"/login"} className="text-lg text-white capitalize">
-								Login
-							</Link>
-						</li>
-					)}
-				</ul>
-			</div>
-		</header>
-	);
+  return (
+    <header className="p-5 z-10 max-w-[1440px] mx-auto text-lavender_bush">
+      <nav className="flex justify-between items-center">
+        <Link to={"/events"} className=" scale-125">
+          ACN.E
+        </Link>
+        {/* Desktop Navigation */}
+        <ul className="flex justify-center items-center gap-16 max-lg:hidden relative">
+          {navLinks.map((item) => (
+            <li
+              key={item?.label}
+              onMouseEnter={() => setHovered(item.href)}
+              onMouseLeave={() => setHovered(null)}
+              className="relative"
+            >
+              <Link
+                to={item?.href}
+                className={` text-lg text-white capitalize`}
+              >
+                {item?.label}
+              </Link>
+              <span
+                className={`absolute left-0 bottom-[-8px] h-[5px] rounded-full bg-purple-500 transition-all duration-300 ${
+                  location?.pathname === item?.href || hovered === item?.href
+                    ? "w-full"
+                    : "w-0"
+                } ${location?.pathname === item?.href ? "bg-white" : ""}`}
+              ></span>
+            </li>
+          ))}
+          {user ? (
+            <UserMenu />
+          ) : (
+            <li
+              onMouseEnter={() => setHovered("/login")}
+              onMouseLeave={() => setHovered(null)}
+              className="relative"
+            >
+              <Link to={"/login"} className="text-lg text-white capitalize">
+                Login
+              </Link>
+              <span
+                className={`absolute left-0 bottom-[-8px] h-[5px] rounded-full bg-purple-500 transition-all duration-300 ${
+                  hovered === "/login" ? "w-full" : "w-0"
+                }`}
+              ></span>
+            </li>
+          )}
+        </ul>
+        {/* Hamburger Menu Button */}
+        <div className="hidden max-lg:block">
+          <button
+            className="text-white text-lg"
+            onClick={() => setMenuOpen(!menuOpen)}
+          >
+            ☰
+          </button>
+        </div>
+      </nav>
+      {/* Sliding Full-Screen Menu */}
+      <div
+        className={`fixed top-0 left-0 z-50 h-full w-full bg-[#1f1f1f] text-white p-5 transform transition-transform duration-300 ${
+          menuOpen ? "translate-x-0" : "-translate-x-full"
+        }`}
+      >
+        <button
+          className="text-lg font-bold absolute top-4 right-4"
+          onClick={() => setMenuOpen(false)}
+        >
+          ✕
+        </button>
+        <ul className="flex flex-col items-center justify-evenly h-full gap-6">
+          {navLinks.map((item) => (
+            <li key={item?.label} className="text-lg w-full">
+              <Link
+                to={item?.href}
+                className={`capitalize hover:text-purple-500 ${
+                  location?.pathname === item?.href
+                    ? "text-purple-500 font-bold"
+                    : ""
+                }`}
+                onClick={() => setMenuOpen(false)} // Close menu on link click
+              >
+                {item?.label}
+              </Link>
+            </li>
+          ))}
+          {user ? (
+            <li className="w-full">
+              <button
+                className="text-red-50 bg-red-950 p-2 w-full text-lg"
+                onClick={handleLogout}
+              >
+                sign out
+              </button>
+            </li>
+          ) : (
+            <li
+              onMouseEnter={() => setHovered("/login")}
+              onMouseLeave={() => setHovered(null)}
+              className="relative"
+            >
+              <Link to={"/login"} className="text-lg text-white capitalize">
+                Login
+              </Link>
+            </li>
+          )}
+        </ul>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
--- src/screen/About.jsx
+++ src/screen/About.jsx
@@ -5,121 +5,136 @@ import { FaLinkedin } from "react-icons/fa";
 import { MdOutlineSupportAgent } from "react-icons/md";
 import { Mutation, useMutation } from "@tanstack/react-query";
 const About = () => {
-	return (
-		<div className="text-white min-h-screen p-2">
-			<Header />
-			<section className="p-4 text-center flex flex-col gap-3 ">
-				<h1 className="font-bold text-3xl sm:text-[32px] md:text-[40px] text-blue-400">
-					About Us
-				</h1>
-				<p className="text-sm sm:text-[16px] ">
-					Welcome to <span className="font-bold text-green-400 text-lg">Alcognerd</span>, your
-					all-in-one event management platform designed to simplify planning, organizing, and
-					executing events seamlessly.
-				</p>
-			</section>
+  return (
+    <div className="text-white min-h-screen p-2">
+      <Header />
+      <section className="p-4 text-center flex flex-col gap-3 ">
+        <h1 className="font-bold text-3xl sm:text-[32px] md:text-[40px] text-blue-400">
+          About Us
+        </h1>
+        <p className="text-sm sm:text-[16px] ">
+          Welcome to{" "}
+          <span className="font-bold text-green-400 text-lg">Alcognerd</span>,
+          your all-in-one event management platform designed to simplify
+          planning, organizing, and executing events seamlessly.
+        </p>
+      </section>
 
-			<hr className="w-3/5 md:w-2/5 mx-auto opacity-55 my-7 md:my-10 border-gray-400 border-[1.5px]" />
-			<section className="px-4 pt-2 text-center flex flex-col gap-3">
-				<h2 className="font-bold text-3xl sm:text-[32px] md:text-[40px] text-blue-400">
-					Our Mission
-				</h2>
-				<p className="text-sm mb-4 sm:text-[16px]">
-					We aim to empower event organizers, businesses, and individuals by providing an
-					intuitive, feature-rich platform that makes event planning effortless.
-				</p>
-				<p className="text-sm pb-4  sm:text-[16px]">
-					From ticket booking to attendee management, we streamline the process so you can
-					focus on creating memorable experiences.
-				</p>
-			</section>
-			<hr className="w-3/5 md:w-2/5 mx-auto opacity-55 my-7 md:my-10 border-gray-400 border-[1.5px]" />
-			<section className="pt-4">
-				<div className="text-center">
-					<h2 className="font-bold text-2xl mb-3 sm:mb-5 sm:text-[40px] text-blue-400">
-						What We Offer
-					</h2>
-				</div>
-				<div className="p-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 sm:gap-8">
-					<article className="flex flex-col items-center group text-center gap-2 sm:gap-4 border-gray-400 border-2 rounded-md p-2 sm:p-6 bg-gray-500 hover:bg-gray-600 transition">
-						<h3 className="font-semibold group-hover:underline group-hover:transition">
-							✅ Seamless Event Creation
-						</h3>
-						<p className="text-sm">Organize public or private events with ease.</p>
-					</article>
-					<article className="flex flex-col items-center group text-center gap-2 sm:gap-4 border-gray-400 border-2 rounded-md p-2 sm:p-6 bg-gray-500 hover:bg-gray-600 transition">
-						<h3 className="font-semibold group-hover:underline group-hover:transition">
-							✅ Secure Online Payments
-						</h3>
-						<p className="text-sm">
-							Integrated payment gateway for hassle-free transactions.
-						</p>
-					</article>
-					<article className="flex flex-col items-center group text-center gap-2 sm:gap-4 border-gray-400 border-2 rounded-md p-2 sm:p-6 bg-gray-500 hover:bg-gray-600 transition">
-						<h3 className="font-semibold group-hover:underline group-hover:transition">
-							✅ Real-time Attendance Tracking
-						</h3>
-						<p className="text-sm">QR-based check-ins for smooth event entry.</p>
-					</article>
-					<article className="flex flex-col items-center group text-center gap-2 sm:gap-4 border-gray-400 border-2 rounded-md p-2 sm:p-6 bg-gray-500 hover:bg-gray-600 transition">
-						<h3 className="font-semibold group-hover:underline group-hover:transition">
-							✅ Task & Team Management
-						</h3>
-						<p className="text-sm">Assign roles and track progress effortlessly. </p>
-					</article>
-					<article className="flex flex-col items-center group text-center gap-2 sm:gap-4 border-gray-400 border-2 rounded-md p-2 sm:p-6 bg-gray-500 hover:bg-gray-600 transition">
-						<h3 className="font-semibold group-hover:underline group-hover:transition">
-							✅ Customizable Event Pages
-						</h3>
-						<p className="text-sm">Showcase your event with stunning visuals and details.</p>
-					</article>
-					<article className="flex flex-col items-center group text-center gap-2 sm:gap-4 border-gray-400 border-2 rounded-md p-2 sm:p-6 bg-gray-500 hover:bg-gray-600 transition">
-						<h3 className="font-semibold group-hover:underline group-hover:transition">
-							✅ Free to edit
-						</h3>
-						<p className="text-sm">Edit all the data you have created.</p>
-					</article>
-				</div>
-			</section>
-			<section className="text-lg font-mono text-center my-7 md:my-10 sm:text-2xl">
-				<p>Join us and take your events to the next level with Alcognerd </p>
-			</section>
-			<section className="flex flex-col gap-4 justify-center">
-				<h2 className="font-bold text-center text-3xl sm:text-[32px] md:text-[40px] text-blue-400">
-					Contact Us
-				</h2>
+      <hr className="w-3/5 md:w-2/5 mx-auto opacity-55 my-7 md:my-10 border-gray-400 border-[1.5px]" />
+      <section className="px-4 pt-2 text-center flex flex-col gap-3">
+        <h2 className="font-bold text-3xl sm:text-[32px] md:text-[40px] text-blue-400">
+          Our Mission
+        </h2>
+        <p className="text-sm mb-4 sm:text-[16px]">
+          We aim to empower event organizers, businesses, and individuals by
+          providing an intuitive, feature-rich platform that makes event
+          planning effortless.
+        </p>
+        <p className="text-sm pb-4  sm:text-[16px]">
+          From ticket booking to attendee management, we streamline the process
+          so you can focus on creating memorable experiences.
+        </p>
+      </section>
+      <hr className="w-3/5 md:w-2/5 mx-auto opacity-55 my-7 md:my-10 border-gray-400 border-[1.5px]" />
+      <section className="pt-4">
+        <div className="text-center">
+          <h2 className="font-bold text-2xl mb-3 sm:mb-5 sm:text-[40px] text-blue-400">
+            What We Offer
+          </h2>
+        </div>
+        <div className="p-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 sm:gap-8">
+          <article className="flex flex-col items-center group text-center gap-2 sm:gap-4 border-gray-400 border-2 rounded-md p-2 sm:p-6 bg-gray-500 hover:bg-gray-600 transition">
+            <h3 className="font-semibold group-hover:underline group-hover:transition">
+              ✅ Seamless Event Creation
+            </h3>
+            <p className="text-sm">
+              Organize public or private events with ease.
+            </p>
+          </article>
+          <article className="flex flex-col items-center group text-center gap-2 sm:gap-4 border-gray-400 border-2 rounded-md p-2 sm:p-6 bg-gray-500 hover:bg-gray-600 transition">
+            <h3 className="font-semibold group-hover:underline group-hover:transition">
+              ✅ Secure Online Payments
+            </h3>
+            <p className="text-sm">
+              Integrated payment gateway for hassle-free transactions.
+            </p>
+          </article>
+          <article className="flex flex-col items-center group text-center gap-2 sm:gap-4 border-gray-400 border-2 rounded-md p-2 sm:p-6 bg-gray-500 hover:bg-gray-600 transition">
+            <h3 className="font-semibold group-hover:underline group-hover:transition">
+              ✅ Real-time Attendance Tracking
+            </h3>
+            <p className="text-sm">
+              QR-based check-ins for smooth event entry.
+            </p>
+          </article>
+          <article className="flex flex-col items-center group text-center gap-2 sm:gap-4 border-gray-400 border-2 rounded-md p-2 sm:p-6 bg-gray-500 hover:bg-gray-600 transition">
+            <h3 className="font-semibold group-hover:underline group-hover:transition">
+              ✅ Task & Team Management
+            </h3>
+            <p className="text-sm">
+              Assign roles and track progress effortlessly.{" "}
+            </p>
+          </article>
+          <article className="flex flex-col items-center group text-center gap-2 sm:gap-4 border-gray-400 border-2 rounded-md p-2 sm:p-6 bg-gray-500 hover:bg-gray-600 transition">
+            <h3 className="font-semibold group-hover:underline group-hover:transition">
+              ✅ Customizable Event Pages
+            </h3>
+            <p className="text-sm">
+              Showcase your event with stunning visuals and details.
+            </p>
+          </article>
+          <article className="flex flex-col items-center group text-center gap-2 sm:gap-4 border-gray-400 border-2 rounded-md p-2 sm:p-6 bg-gray-500 hover:bg-gray-600 transition">
+            <h3 className="font-semibold group-hover:underline group-hover:transition">
+              ✅ Free to edit
+            </h3>
+            <p className="text-sm">Edit all the data you have created.</p>
+          </article>
+        </div>
+      </section>
+      <section className="text-lg font-mono text-center my-7 md:my-10 sm:text-2xl">
+        <p>Join us and take your events to the next level with Alcognerd </p>
+      </section>
+      <section className="flex flex-col gap-4 justify-center">
+        <h2 className="font-bold text-center text-3xl sm:text-[32px] md:text-[40px] text-blue-400">
+          Contact Us
+        </h2>
 
-				<p className="text-sm text-center md:text-lg">
-					We’d love to hear from you! Whether you have questions, feedback, or need assistance,
-					feel free to reach out.
-				</p>
-				<div className="flex flex-col sm:flex-row justify-around p-2 my-3 gap-8">
-					<div className="opacity-75 flex flex-col gap-4">
-						<h3>🌐 Follow Us on Social Media</h3>
-						<p>
-							🔹 <FaLinkedin className="inline" />{" "}
-							<a href="https://www.google.com" target="_blank">
-								/alcognerd
-							</a>
-						</p>
-						<h3 className="pl-1 mt-3">
-							<MdOutlineSupportAgent className="inline text-blue-300 text-lg " />
-							<span className="pl-2">Support</span>
-						</h3>
-						<p>🔹📩 help.alcognerd@gmail.com</p>
-					</div>
-					<ContactForm />
-				</div>
-			</section>
-			<div className="p-3 sm:text-[16px] text-center my-4">
-				We're here to help and make your event management experience smoother than ever! --- Let
-				me know if you need modifications or additional details!
-			</div>
-			<div className="text-center text-sm my-4">
-				<p>&copy;alcognerd-2025 All rights reserved</p>
-			</div>
-		</div>
-	);
+        <p className="text-sm text-center md:text-lg">
+          We’d love to hear from you! Whether you have questions, feedback, or
+          need assistance, feel free to reach out.
+        </p>
+        <div className="flex flex-col sm:flex-row justify-around p-2 my-3 gap-8">
+          <div className="opacity-75 flex flex-col gap-4">
+            <h3>🌐 Follow Us on Social Media</h3>
+            <p>
+              🔹 <FaLinkedin className="inline" />{" "}
+              <a
+                href="
+https://www.linkedin.com/in/alcognerd-world-b2631b351?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"
+                target="_blank"
+              >
+                /alcognerd
+              </a>
+            </p>
+            <h3 className="pl-1 mt-3">
+              <MdOutlineSupportAgent className="inline text-blue-300 text-lg " />
+              <span className="pl-2">Support</span>
+            </h3>
+            <p>🔹📩 help.alcognerd@gmail.com</p>
+          </div>
+          <ContactForm />
+        </div>
+      </section>
+      <div className="p-3 sm:text-[16px] text-center my-4">
+        We're here to help and make your event management experience smoother
+        than ever! --- Let me know if you need modifications or additional
+        details!
+      </div>
+      <div className="text-center text-sm my-4">
+        <p>&copy;alcognerd-2025 All rights reserved</p>
+      </div>
+    </div>
+  );
 };
 
 export default About;
